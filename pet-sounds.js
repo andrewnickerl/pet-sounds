ef3426d16e6e2fcb528a3cb5e21eb63187b4c820
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-01-13 06:15:54 -0500

    Create pet-sounds.js and README.md

---
+++ pet-sounds.js
@@ -0,0 +1,50 @@
+let animalNoises = [
+  { 'dog': {
+    'America' : 'Woof! Woof!',
+    'Germany' : 'Wau Wau!',
+    'England' : 'Bow wow!',
+    'Uruguay' : 'Jua jua!',
+    'Afrikaans' : 'Blaf!',
+    'Korea' : 'Mong mong!',
+    'Iceland' : 'Voff voff!',
+    'Albania' : 'Ham!',
+    'Algeria' : 'Ouaf ouaf!'
+    }
+  },
+  { 'cat': {
+    'America' : 'Meow',
+    'Germany' : 'Miauw!',
+    'England' : 'mew mew',
+    'Uruguay' : 'Miau Miau!',
+    'Afrikaans' : 'Purr',
+    'Korea' : 'Nyaong!',
+    'Iceland' : 'Kurnau!',
+    'Albania' : 'Miau',
+    'Algeria' : 'Miaou!'
+    }
+  },
+  { 'chicken': {
+    'America' : 'Cluck cluck',
+    'Germany' : 'tock tock tock',
+    'England' : 'Cluck Cluck',
+    'Uruguay' : 'gut gut gdak',
+    'Afrikaans' : 'kukeleku',
+    'Korea' : 'ko-ko-ko',
+    'Iceland' : 'Chuck-chuck!',
+    'Albania' : 'Kotkot',
+    'Algeria' : 'Cotcotcodet'
+    }
+  }
+];
+
+const petSounds = (animal, country) => {
+  let noise = ''
+  let capAnimal = animal.charAt(0).toUpperCase() + animal.slice(1)
+  for(let i=0; i<animalNoises.length; i++) {
+    if(!!animalNoises[i][`${animal}`]) {
+      noise = animalNoises[i][`${animal}`][`${country}`]
+    }
+  }
+  return capAnimal + 's in ' + country + ' say ' + noise;
+}
+console.log(petSounds('dog', 'Iceland'))
